32ee119d79aa665a199e48b5f7830088dc70f2dd
moham <mohammedrizutk35@gmail.com>
2023-06-13 00:52:15 +0530

    latest pull

--- lib/constants/api_helpers.dart
+++ lib/constants/api_helpers.dart
@@ -1,14 +1,7 @@
- class ApiHelpers {
-
- static const baseUrl = "https://api.openai.com/v1";
-  static const apikeys = "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";
-
-
+class ApiHelpers {
+  static const baseUrl = "https://api.openai.com/v1";
+  static const apikeys = "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";
 }
 
-
-  const baseUrl = "https://api.openai.com/v1";
-   const apikeys = "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";
-
-
-
+const baseUrl = "https://api.openai.com/v1";
+const apikeys = "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";
--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,4 +1,5 @@
-
+import 'dart:convert';
+import 'dart:developer';
 
 import 'package:chat_gpt_application/constants/api_helpers.dart';
 import 'package:chat_gpt_application/models/aiModels_response_model.dart';
@@ -8,33 +9,38 @@ import 'package:http/http.dart' as http;
 class ApiServices {
   final _dio = Dio(BaseOptions(
       baseUrl: ApiHelpers.baseUrl,
-      headers: {"Authorization": "Bearer${ApiHelpers.apikeys}"}));
+      headers: {"Authorization": "Bearer ${ApiHelpers.apikeys}"}));
 
   getAiModels() async {
     print("function called");
     try {
       final response = await _dio.get("/models");
 
-      print(response.data);
+      final resultList =
+          AimodelsResponseModel.fromJson(response.data).data?.toList();
+
+      var list = [];
+
+      for (var i in resultList!) {
+        list.add(i.id);
+      }
+
+      print(list);
+
+      return list;
     } catch (e) {
       print(e);
     }
   }
 
-
   static getAiModelsHttp() async {
-    
-
     try {
       var response = await http.get(Uri.parse("$baseUrl/models"),
-      headers:{'Authorization': 'Bearer $apikeys'} );
+          headers: {'Authorization': 'Bearer $apikeys'});
 
       print(response.body);
     } catch (e) {
-
       print(e);
-      
     }
-
   }
 }
