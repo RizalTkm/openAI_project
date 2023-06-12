84765e01040fa5c67e1b17778d01cc738853db4b
RizalTkm <101650401+RizalTkm@users.noreply.github.com>
2023-06-13 00:58:11 +0530

    Delete api_helpers.dart

--- lib/constants/api_helpers.dart
+++
@@ -1,7 +0,0 @@
-class ApiHelpers {
-  static const baseUrl = "https://api.openai.com/v1";
-  static const apikeys = "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";
-}
-
-const baseUrl = "https://api.openai.com/v1";
-const apikeys = "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";
